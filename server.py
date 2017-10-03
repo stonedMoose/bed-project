291a7d389dff6ee195764797fb7988529bde2db6
bantoniotti <benoit.antoniotti@insa-lyon.fr>
2017-10-03 16:17:14 +0000

    added server.py

--- server.py
+++ server.py
@@ -3,30 +3,7 @@
 
 import socket, threading,signal,sys
 
-def signal_handler(signal,frame):
-	print "Close"
-	serverSocket.close()
-	for i in threads:
-		i.stop()
-	sys.exit(0)
 
-
-
-#def changetemp(signal,frame):
-
-
-signal.signal(signal.SIGINT,signal_handler)
-#signal.signal(signal.SIGQUIT,changetemp)	
-serverPort = 4242 
-
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serverSocket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-serverSocket.bind(('',serverPort))
-baseTemp= float(raw_input("Please enter the temperature you wish in the room\n"))
-print("you want "+str(baseTemp)+" degree")
-
-
-threads=[]
 
 class ClientHandler(threading.Thread):
         def __init__(self,ip,port,socket):
@@ -44,6 +21,8 @@ class ClientHandler(threading.Thread):
 		return self.__stop__event.is_set()
 
 
+              
+                
         def run(self):
                 print "\n Connection from : "+ip+" : "+str(port)+" with socket"+str(self.socket.getsockname())
                 data="dump"
@@ -53,19 +32,64 @@ class ClientHandler(threading.Thread):
 				self.stop()
 				break
 			room=data[0]
-			sensor=data[1]
-			temp=(float(data[3:])/10.0)
-                 	print "client "+ ip+":" +str(port)+" : room "+room+" sensor "+sensor+" temp "+str(temp)  
-			if temp<baseTemp:
-				resp="1 "+str(baseTemp-temp) #1=up
-				self.socket.send(resp)
-			if temp>baseTemp:
-				resp="2 "+str(temp-baseTemp)#2=down
-				self.socket.send(resp)
+			#test
+			if int(room)==0:
+                                sensor="0"
+                                temp=data[2:]
+                        else:
+                                sensor=data[1]
+                                temp=data[3:]
+                 	print "client "+ ip+":" +str(port)+" : room "+room+" sensor "+sensor+" temp "+temp  
+                        #write temp
+                        name="capteur"+room+sensor+".txt"
+                        file=open(name,"a")
+                        file.write(temp+"\n");
+                        file.close()
+                        #response
+                 	temp.replace(",",".")
+                 	temp=float(temp)
+                        if int(room)==1:
+                                 baseTemp=baseTemp1
+                        elif int(room)==2:
+                                 baseTemp=baseTemp2
+                        if temp<baseTemp:
+                                resp="1 "+str(baseTemp-temp) #1=up
+                                self.socket.send(resp)
+                        if temp>baseTemp:
+                                resp="2 "+str(temp-baseTemp)#2=down
+                                self.socket.send(resp)self.sendResp(int(room),float(temp))       
                 print " Client disconnected"       
                 self.socket.close()        
                         
-   
+
+def signal_handler(signal,frame):
+	print "Close"
+	serverSocket.close()
+	for i in threads:
+		i.stop()
+	sys.exit(0)
+
+
+
+def setBaseTemp(newTemp,salle):
+       if salle==1:
+               baseTemp1=newTemp
+       elif salle==2:
+               baseTemp2=newTemp
+
+
+signal.signal(signal.SIGINT,signal_handler)
+serverPort = 4242 
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+serverSocket.bind(('',serverPort))
+
+baseTemp1= setBaseTemp(30,1)
+baseTemp2= setBaseTemp(28,2)
+
+
+threads=[] 
 
 while True :
 	try:
