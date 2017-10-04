0b3cef839f2ca9af08389730a8695527bde6c990
Julien Lhermite <julien.lhermite@gmail.com>
2017-10-04 11:20:57 +0200

    hierarchy refactoring

--- ez430futur/inc/watchdog.h
+++
@@ -1,9 +0,0 @@
-#ifndef MSP430_WATCHDOG_H
-#define MSP430_WATCHDOG_H
-
-/* save watchdog configuration and stop it */
-void watchdog_stop();
-/* restore the last saved watchdog configuration */
-void watchdog_restore();
-
-#endif
