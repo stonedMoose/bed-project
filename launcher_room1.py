5e28807e24349a6e92b71965af43d0b3395b3a27
bantoniotti <benoit.antoniotti@insa-lyon.fr>
2017-10-04 08:56:51 +0200

    added second queue

--- launcher_room1.py
+++ launcher_room1.py
@@ -17,12 +17,13 @@ signal(SIGINT, interrupt)
 
 # cree une queue de taille 10
 q = Queue(10)
+q2 = Queue(10)
 
 parser_process = Process(target=parse, args=(q, 1,))
 parser_process.daemon = True
 parser_process.start()        # Launch reader() as a separate python process
 
-gw_process = Process(target=gateway, args=(q,))
+gw_process = Process(target=gateway, args=(q,q2,))
 gw_process.daemon = True
 gw_process.start()
 
